a336fc1ccb8a07628342a47ab11033b69ab780ef
HappyKumamoto <67646107+HappyKumamoto@users.noreply.github.com>
2021-06-14 15:52:44 +0900

    Update CheshireCat.js

--- CheshireCat.js
+++ CheshireCat.js
@@ -1,8 +1,9 @@
-let header = document.getElementById("header");
+let neko1 = document.getElementById("neko1");
+let neko2 = document.getElementById("neko2");
 let isPositive = true;
 let opacity = 0; // 不透明度
 
-function opacityAnimate() {
+function opacityNeko1() {
   if (isPositive) {
     opacity += 0.05;
   } else {
@@ -15,9 +16,26 @@ function opacityAnimate() {
     isPositive = true;
   }
 
-  header.style.opacity = opacity;
+  neko1.style.opacity = opacity;
 }
 
-// 300ミリ秒ごとに opacityAnimate 関数を実行する
-setInterval(opacityAnimate, 300);
+function opacityNeko2() {
+  if (isPositive) {
+    opacity += 0.05;
+  } else {
+    opacity -= 0.05;
+  }
 
+  if (opacity > 1) {
+    isPositive = false;
+  } else if (opacity < 0) {
+    isPositive = true;
+  }
+
+  neko2.style.opacity = opacity;
+}
+
+
+// 1000ミリ秒ごとに opacityAnimate 関数を実行する
+setInterval(opacityNeko1, 1000);
+setInterval(opacityNeko2, 1000);
